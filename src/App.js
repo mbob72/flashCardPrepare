07ff73278ea3c47fe81eb87aef3b36375d51c6ec
Mikhail Bobryshev <MBobryshev@luxoft.com>
2021-06-16 21:08:05 +0300

    template complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { dd } from "./table"
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+const fonts = {
+    Times: {
+        normal: 'https://localhost:3000/lsansuni.ttf',
+        bold: 'https://localhost:3000/lsansuni.ttf',
+        italics: 'https://localhost:3000/lsansuni.ttf',
+        bolditalics: 'https://localhost:3000/lsansuni.ttf'
+    },
+    Roboto: {
+        normal: 'https://localhost:3000/7454.ttf',
+        bold: 'https://localhost:3000/7454.ttf',
+        italics: 'https://localhost:3000/7454.ttf',
+        bolditalics: 'https://localhost:3000/7454.ttf'
+    },
+}
 
 function App() {
+
+  function click() {
+      console.log('start::')
+    let win = window.open('', '_blank');
+    pdfMake.createPdf(dd, null, fonts).open({}, win);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" onClick={click}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
